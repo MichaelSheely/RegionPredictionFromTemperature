bab7dd1de6f37750d43a2dce58634889cdbf5547
Lisa Yin <lyin@g.hmc.edu>
2017-04-01 13:37:24 -0700

    Get City and State

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 import pandas as pd
+import os.path
+
+TEMPERATURES_FILE = 'data/USCityTemperaturesAfter1850.csv'
+CITY_STATE_FILE = 'data/city_state.csv'
 
 def load_data(path='data/GlobalLandTemperaturesbyCity.csv', ignore_before=1850):
-    out = pd.read_csv("data/GlobalLandTemperaturesbyCity.csv", header=0)
+    out = pd.read_csv(path, header=0)
     us = out.loc[out['Country'] == 'United States']
     us = us.loc[us['dt'] > 1850]
-    us.to_csv('data/USCityTemperaturesAfter1850.csv')
+    us.to_csv(TEMPERATURES_FILE)
     return us
 
+def city_country(raw_file='data/RawUSData.csv'):
+    out = pd.read_csv(raw_file)
+    keep = ['Name', 'Canonical Name']
+    us = out[keep]
+    us = us.assign(State = us['Canonical Name'].apply(get_state))
+    us = us.rename(columns={'Name':'City'})
+    us = us[['City', 'State']]
+    us.to_csv(CITY_STATE_FILE)
+
+def get_state(raw_string):
+    return raw_string.split(',')[-2]
 
 def main():
-    data = pd.read_csv('data/USCityTemperaturesAfter1850.csv')
+    if not os.path.isfile(TEMPERATURES_FILE): # TODO: add force make file
+        load_data()
+    data = pd.read_csv(TEMPERATURES_FILE)
+    city_country()
 
 if __name__ == "__main__":
     main()

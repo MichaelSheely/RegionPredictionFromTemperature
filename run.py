08d9b6a3c66dc92f64f7e4ecac08efce7de617d7
Lisa Yin <lyin@g.hmc.edu>
2017-04-16 14:41:04 -0700

    hopefully indices are ok

--- run.py
+++ run.py
@@ -15,10 +15,12 @@ cities_dict = {}
 counter = 0
 def number_cities(row):
     global counter
-    if not row in cities_dict:
-        cities_dict[row] = counter
+    key = (row[0],row[1])
+    if not key in cities_dict:
+        print key
+        cities_dict[key] = counter
         counter += 1
-    return cities_dict[row]
+    return cities_dict[key]
 
 def number_regions(row):
     if row == 'Northeast':
@@ -36,45 +38,66 @@ def number_regions(row):
 
 def split_data(df, city_regions):
     # Get the mapping from each unique city to each region
-    orig_cities = city_regions['City']
-    X_cities = orig_cities.index #orig_cities.apply(number_cities)
+    orig_cities = city_regions[['City','State']]
+    print "Total cities ", len(orig_cities)
     y_regions = city_regions['Region']
     y_regions = y_regions.apply(number_regions)
 
     y_train, y_test = train_test_split(y_regions)
-    df_train = df.loc[df.City.isin(y_train.index)]
-    df_test = df.loc[df.City.isin(y_test.index)]
+    df_train = df.loc[df.CityIndex.isin(y_train.index)]
+    df_test = df.loc[df.CityIndex.isin(y_test.index)]
     X_train = pd.DataFrame(index=y_train.index)
     X_test = pd.DataFrame(index=y_test.index)
-    train_names = orig_cities[y_train.index]
-    test_names = orig_cities[y_test.index]
+    print len(orig_cities)
+    print len(y_train.index)
+    print orig_cities
+    print y_train.index
+    print max(y_train.index)
+    train_names = orig_cities.iloc[y_train.index]
+    test_names = orig_cities.iloc[y_test.index]
 
     train = {'df': df_train, 'X': X_train, 'y': y_train, 'city_names': train_names}
     test = {'df': df_test, 'X': X_test, 'y': y_test, 'city_names': test_names}
     return (train, test)
 
-def run(filename='data/joined.csv', city_regions_file='data/CityRegions.csv'):
+def run(filename='data/clean_data.csv', city_regions_file='data/CityRegions.csv'):
     df = pd.read_csv(filename, header=0)
     df.dropna(inplace=True)
 
-    X_labels = ['City', 'dt', 'AverageTemperature']
+    X_labels = ['City', 'State', 'dt', 'AverageTemperature', 'CityIndex']
     df = df[X_labels]
     df = df.dropna()
-    df['City'] = df['City'].apply(number_cities)
-
+    city_state = df[['City', 'State']]
+    # Sadness because multiple cities with same name.......
+    #hi = city_state.apply(number_cities, axis=1)
+    #df['CityIndex'] = city_state.apply(number_cities, axis=1)
+    #df.to_csv('data/clean_data.csv', index=False)
 
     if city_regions_file == None:
         temp = [['Abiline', 'South'],['West Jordon', 'West' ], ['Yonkers', 'Northeast']]
-        city_regions = pd.DataFrame(temp, columns=['City', 'Region'])
+        city_regions = pd.DataFrame(temp, columns=['City','Region'])
     else:
-        city_regions = pd.read_csv(city_regions_file, header=0)
+        city_regions = pd.read_csv(city_regions_file, header=0).reset_index(drop=True)
+        
 
-    print city_regions
     train, test = split_data(df, city_regions)
+    """
+    blah = train['city_names']
+    print blah
+    indices = blah[blah.isin(['Yonkers','Worcester','Winston Salem','Windsor' ,'Wichita' ,'Westminster' ,'West Valley City' ,'West Jordan'])].index
+    print indices
+    print train['df'][train['df']['City'].isin(indices)]
+    print train['df']['City']
+    df = train['df'][train['df']['City'].isin(indices)]
+    X = train['X'][indices]
+    y = train['y'][indices]
+    city_names = train['city_names'][indices]
+    """
 
+    
     feature_extraction_settings = FeatureExtractionSettings()
     feature_extraction_settings.IMPUTE = impute
-    aug = FeatureAugmenter(feature_extraction_settings, column_id='City',
+    aug = FeatureAugmenter(feature_extraction_settings, column_id='CityIndex',
                     column_sort='dt', column_value='AverageTemperature',
                     timeseries_container=train['df'])
     output = aug.fit_transform(train['X'], train['y'])
@@ -88,18 +111,21 @@ def run(filename='data/joined.csv', city_regions_file='data/CityRegions.csv'):
     #                ('classifier', DecisionTreeClassifier(criterion='entropy'))])
 
     # for the fit on the train test set, we set the fresh__timeseries_container to `df_train`
+    """
     pipeline.set_params(augmenter__timeseries_container=train['df'])
     pipeline.fit(train['X'], train['y'])
 
     y_pred = pipeline.predict(train['X'])
     y_true = np.array(train['y'])
     print "train accuracy ", accuracy_score(y_true, y_pred)
-
+    """
     # for the predict on the test test set, we set the fresh__timeseries_container to `df_test`
+    """
     pipeline.set_params(augmenter__timeseries_container=test['df'])
     y_pred = pipeline.predict(test['X'])
 
     y_true = np.array(test['y'])
+    """
 
     print "test accuracy ", accuracy_score(y_true, y_pred)
     print "done"

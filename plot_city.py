239decbedb219393558583b10fee1ac44d57d666
Michael Sheely <msheely@hmc.edu>
2017-04-15 20:09:12 -0700

    Added generality to which city to plot

--- plot_city.py
+++ plot_city.py
@@ -1,15 +1,22 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import sys
 
 df = pd.read_csv('data/joined.csv', header=0)
 df.dropna(inplace=True)
 
-fresno_frame = df[df['City'] == 'Fresno']
-ts = Series(fresno_frame['AverageTemperature'], index=fresno_frame['dt'])
+city_name = ""
+if len(sys.argv) < 2:
+    city_name = "Fresno"
+city_name = sys.argv[1]
+
+city_frame = df[df['City'] == city_name]
+ts = pd.Series(city_frame['AverageTemperature'], index=city_frame['dt'])
 ts.cumsum()
 ts.plot()
 
+plt.show()
 
 
 
